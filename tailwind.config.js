25b163e891ff5f454bc08f5d2193793affa93156
unixnexo <unixnexo@gmail.com>
2024-07-18 13:43:14 +0330

    dev

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "soundcloud-main-bg": "#181A1B",
+        "soundcloud-header": "#262A2B",
+        "soundcloud-orange": "#CC5000",
+        "soundcloud-black": "#0D0E0E",
+        "soundcloud-divide": "#454545",
+        "soundcloud-text": "#C8C3BC",
+        "soundcloud-orange-light": "#F1851A",
+      }
+    },
+  },
+  plugins: [],
+}
+
