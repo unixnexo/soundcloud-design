40a52cc822521842b84635b982fd439c160522d0
unixnexo <unixnexo@gmail.com>
2024-07-19 17:43:19 +0330

    dev

--- src/js/script.js
+++ src/js/script.js
@@ -27,6 +27,35 @@ document.querySelectorAll('.black-when-active').forEach(el => {
 
 
 ////////// test //////////////
-document.addEventListener('drag', () => {
-  console.log('draaagin');
+const slider = document.getElementById('slider');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+function updateButtonStates() {
+  // Check if the slider has reached the end of the left
+  if (slider.scrollLeft <= 50) {
+    prevBtn.classList.add('hidden');
+  } else {
+    prevBtn.classList.remove('hidden');
+  }
+
+  // Check if the slider has reached the end of the right
+  if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - 1) {
+    nextBtn.classList.add('hidden');
+  } else {
+    nextBtn.classList.remove('hidden');
+  }
+}
+updateButtonStates();
+
+nextBtn.addEventListener('click', () => {
+  slider.scrollBy({left: 500, behavior: 'smooth'});
+  updateButtonStates();
 });
+
+prevBtn.addEventListener('click', () => {
+  slider.scrollBy({left: -500, behavior: 'smooth'});
+  updateButtonStates();
+});
+
+slider.addEventListener('scroll', () => updateButtonStates());
